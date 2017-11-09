538ca33f0a8ed14d69ab3efc5a9660c021af513f
sgwannabe <33509021+sgwannabe@users.noreply.github.com>
2017-11-09 20:25:32 +0900

    add entries

--- apps/web/controllers/entries/index.rb
+++ apps/web/controllers/entries/index.rb
@@ -3,13 +3,21 @@ module Web::Controllers::Entries
     include Web::Action
 
     expose :entries
-    expose :sort
-
+    expose :sort #not really necessary at this time
+    
     def call(params)      
       
       # @entries = EntryRepository.new.all
       @entries = EntryRepository.new.most_recent
       @sort = params[:sort]
+      
+      #most_recent or least_recent
+      if @sort == 'most_recent'
+        @sort == 'most_recent'
+        @entries == EntryRepository.new.most_recent
+      else
+        @entries == EntryRepository.new.least_recent  
+      end      
     end
   end
 end
